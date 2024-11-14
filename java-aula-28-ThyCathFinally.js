86c53f68153b9cc819ff173a3ae34dd3ce4ef41c
Linux <cristianpu52014@gmail.com>
2024-11-14 20:46:25 -0300

    ThyCathFinally

---
+++ java-aula-28-ThyCathFinally.js
@@ -0,0 +1,28 @@
+// EXEMPLO #1
+/* 
+try {
+ // TENTA EXECUTAR ALGO.
+ console.log(result);
+} catch (error) {
+ // CAPTURA O ERRO PARA TRATAR.
+ console.log(
+  'Não foi possivel executar o seu pedido. Tente novamente mais tarde!'
+ ); 
+ console.log(error);
+} finally {
+ console.log('Fim');
+}
+*/
+
+// EXEMPLO #2
+let result = 0;
+
+try {
+ if (result === 0) {
+  throw new Error('O Valor é igual a zero.');
+ }
+} catch (error) {
+ console.log(error);
+} finally {
+ console.log('Fim');
+}
