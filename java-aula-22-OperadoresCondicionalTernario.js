a3c15ec7de4dd1e0989416ca9849316304b03483
Linux <cristianpu52014@gmail.com>
2024-11-13 22:07:05 -0300

    run

--- java-aula-22-OperadoresCondicionalTernario.js
+++ java-aula-22-OperadoresCondicionalTernario.js
@@ -1,4 +1,14 @@
 // OPERA DOR CONDIOCINAL TERNÁRIO
+let age = 21;
 
-let age = 16;
-console.log(age >= 18 ? 'Você pode dirigir' : 'Você não pode dirigir.');
+console.log(
+  age >= 18 // SE FOR CONDIÇÃO
+    ? 'Você pode Dirigir.' // SE FOR VERDADEIRO
+    : 'Você não pode Dirigir.' // SE FOR FALSO
+);
+
+console.log(
+  age < 18 // SE FOR CONDIÇÃO
+    ? 'Voce não pode Dirigir.' // SE FOR FALSO
+    : 'Voce pode Dirigir.' // SE FOR VERDADEIRO
+);
