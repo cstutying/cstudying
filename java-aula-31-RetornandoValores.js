5b6b5aaf7dffca96600cbd9273b50ec35bb40399
Linux <cristianpu52014@gmail.com>
2024-11-15 00:48:25 -0300

    RetornandoValores

---
+++ java-aula-31-RetornandoValores.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+ let result = a + b; // FAZER A SOMA
+ return result; // RETURNA O RESULTADO
+}
+
+let response = sum(7, 7); // RECEBE O RETORNO DA FUNÇÃO
+console.log(response); // EXIBE O RESULTADO
+
+console.log(sum(5, 6)); //  CHAMA O RETORNO E JAH O USA DENTRO DO CONSOLE
