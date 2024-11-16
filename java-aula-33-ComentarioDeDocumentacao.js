e021b0019c515b56a91073735b0fcf97ee492d93
Linux <cristianpu52014@gmail.com>
2024-11-15 23:07:27 -0300

    ComentarioDeDocumentacao

---
+++ java-aula-33-ComentarioDeDocumentacao.js
@@ -0,0 +1,17 @@
+/* 
+Comentario de documentação em JavaScript (sintaxe de JSDoc) o JSDoc é um padrão para incorporar documentação no código-fonte a partir desses comentários.
+*/
+
+/**
+ * Authenticates the user
+ *
+ * @param {string} email user email
+ * @param {string} password more than 6 characters
+ * @returns {number} user id
+ */
+
+function signIn(email, password) {
+ // TODO FLUXO DE ATUTENTICAÇÃO DO USUARIO
+ return 7;
+}
+signIn('christian@gmail.com', '123456');
