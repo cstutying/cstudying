1fa548e28979f1367fd0a1470fae1aba3e0e4a4a
Linux <cristianpu52014@gmail.com>
2024-11-16 23:30:28 -0300

    ArrowFunction

---
+++ java-aula-35-ArrowFunction.js
@@ -0,0 +1,14 @@
+// ARROW FUNTION (FUNÇÃO DE SETA)
+
+const showMessage1 = () => {
+ console.log("Ola");
+};
+console.log(showMessage1);
+showMessage1();
+
+const showMessage2 = (username, email) => {
+ console.log("Ola,", username, ". Seu Email é", email); //EXEMPLO 01
+ console.log(`Ola, ${username}. Seu email é: ${email}.`); //EXEMPLO 02
+};
+
+showMessage2("Douglas", "login@gmail.com");
