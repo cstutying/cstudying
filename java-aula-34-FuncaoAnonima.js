34f0c08b5d47bec47a94b54e4960c872330e154b
Linux <cristianpu52014@gmail.com>
2024-11-16 23:13:43 -0300

    FuncaoAnonima

--- java-aula-34-FuncaoAnonima.js
+++ java-aula-34-FuncaoAnonima.js
@@ -1,1 +1,20 @@
 // FUNÇÃO ANONIMA (FUNÇÃO QUE NÃO POSSUI NOME)
+
+// EXEMPL O1
+const showMessage1 = function () {
+ return "Ola, Christian";
+};
+console.log(showMessage1());
+
+// EXEMPL O2
+const showMessage2 = function (name) {
+ return "Ola, " + name;
+};
+
+console.log(showMessage2("Douglas"));
+
+// EXEMPLO 03
+const showMessage3 = function (message, name) {
+ return message + name;
+};
+console.log(showMessage3("Ola, ", "Dias"));
