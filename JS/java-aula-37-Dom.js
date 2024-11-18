e3aa69ef799371bc7605f504b230dd9b99f72c38
Linux <cristianpu52014@gmail.com>
2024-11-17 22:08:03 -0300

    next

---
+++ JS/java-aula-37-Dom.js
@@ -0,0 +1,26 @@
+// DOM
+
+// VISUALIZAR O CONTEUDO DO DOCUMENT
+// console.log(document);
+
+// OBTER O TITLE DA PAGINA
+console.log(document.title);
+
+// ACESSAR O ELEMENTO PELO ID (SELETOR ID)
+const guest = document.getElementById("guest-2");
+console.log(guest);
+
+// MOSTRA AS PROPRIEDADES DO OBJETOS
+console.dir(guest);
+
+// ACESSAR ELEMENTO COM CLASS (SELETOR CLASS)
+const guestsByClass = document.getElementsByClassName("guest");
+console.log(guestsByClass);
+
+// EXIBIR O PRIMEIRO ELEMENTO DA LISTA
+console.log(guestsByClass.item(0)); // É POSSIVEL USAR ESSA OPÇÃO 1
+console.log(guestsByClass[0]); // OU ESSA OPÇÃO 2
+
+// SELECIONAR LISTA DE ELEMENTO PELA TAG
+const guestsTag = document.getElementsByTagName("li");
+console.log(guestsTag);
