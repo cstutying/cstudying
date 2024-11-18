3cfd6370bd95729466de6b77326738540042f2f5
Linux <cristianpu52014@gmail.com>
2024-11-17 22:57:29 -0300

    QuerrySelector

---
+++ JS/java-aula-38-QuerrySelector.js
@@ -0,0 +1,10 @@
+// ACESSA O ELEMENTO PELO SELECTOR ID
+const guest = document.querySelector("#guest-2");
+console.log(guest);
+
+// ACESSA O ELEMENTO PELO SELECTOR CLASS
+// const guests = document.querySelector(".guest");
+
+// RETORNA TODOS OS ELEMENTO ENCONTRADOS PELO QUERY
+const guests = document.querySelectorAll(".guest");
+console.log(guests);
