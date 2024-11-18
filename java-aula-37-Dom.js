e4a638f7cbba8cfffaccee71477ea65347cf8efc
Linux <cristianpu52014@gmail.com>
2024-11-17 21:12:13 -0300

    Dom

---
+++ java-aula-37-Dom.js
@@ -0,0 +1,13 @@
+// DOM
+// VISUALIZAR O CONTEUDO DO DOCUMENT
+// console.log(document);
+
+// OBTER O TITLE DA PAGINA
+console.log(document.title);
+
+// ACESSAR O ELEMENTO PELO ID (SELETOR ID)
+const guest = document.getElementById("guest-2");
+console.log(guest);
+
+// MOSTRA AS PROPRIEDADES DO OBJETOS
+console.dir(guest);
