6b54f3dab57c3db67236f7d62cc4ae2a5bca9cfe
Linux <cristianpu52014@gmail.com>
2024-11-18 01:16:14 -0300

    run

--- C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a28aa15bd6e65d1f9d49f376440762191b260d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb9e6cc7285befa5c9b2d0a19ed211b590c909a")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
