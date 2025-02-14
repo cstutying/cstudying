f045d1b382237ec89955fb5caf029d0194651466
cstutying <cristianpu52014@gmail.com>
2025-02-14 01:32:02 -0300

    .

--- CSharp/obj/Debug/net9.0/CSharp.AssemblyInfo.cs
+++ CSharp/obj/Debug/net9.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd5f13e866c92f48aa3d43eae9aa57cb7bfb9a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e366c8aeef16c941ebd407e22f51ef1a8db8e618")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
